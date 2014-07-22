c4bf324700852b181bc31d65764f1a1eff016e5c
Nico Hammerl <nico.hammerl@gmaill.com>
2014-07-23 01:20:04 +0200

    added output per txt function

--- TTRecorder/ViewModel/MainViewModel.cs
+++ TTRecorder/ViewModel/MainViewModel.cs
@@ -1,9 +1,16 @@
 ﻿using nhammerl.TTRecorder.Annotations;
 using nhammerl.TTRecorder.Model;
 using nhammerl.TTRecorder.ViewModel.Command;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
 
 namespace nhammerl.TTRecorder.ViewModel
 {
@@ -21,6 +28,8 @@ namespace nhammerl.TTRecorder.ViewModel
         public ViewModelCommand CloseInputDialog { get; set; }
 
         public ViewModelCommand CloseDialog { get; set; }
+
+        public ViewModelCommand CreateOutputFile { get; set; }
 
         public ObservableCollection<ITaskViewModel> Tasks { get; set; }
 
@@ -61,6 +70,7 @@ namespace nhammerl.TTRecorder.ViewModel
         public MainViewModel(MainPage mainPage)
         {
             _mainPage = mainPage;
+
             // Init Commands
             PunchIn = new ViewModelCommand
             {
@@ -85,6 +95,12 @@ namespace nhammerl.TTRecorder.ViewModel
                 Text = "X"
             };
 
+            CreateOutputFile = new ViewModelCommand
+            {
+                Command = new RelayCommand(r => GenerateOutputFile()),
+                Text = "Create TXT"
+            };
+
             // Init Taskcollection.
             Tasks = new ObservableCollection<ITaskViewModel>();
         }
@@ -105,6 +121,79 @@ namespace nhammerl.TTRecorder.ViewModel
         public void ShowMessage(string message)
         {
             ShowInputDialog = true;
+        }
+
+        public async void GenerateOutputFile()
+        {
+            var savePicker = new FileSavePicker { SuggestedStartLocation = PickerLocationId.DocumentsLibrary };
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = string.Format("TaskTimeRecords_{0}", DateTime.Now.ToString("yyyyMMdd"));
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+
+                // write to file
+                await FileIO.WriteTextAsync(file, BuildTaskInfo());
+
+                // Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                var status = await CachedFileManager.CompleteUpdatesAsync(file);
+
+                if (status == FileUpdateStatus.Complete)
+                {
+                    ShowMessage("File " + file.Name + " was saved.");
+                }
+                else
+                {
+                    ShowMessage("File " + file.Name + " couldn't be saved.");
+                }
+            }
+            else
+            {
+                ShowMessage("Operation cancelled.");
+            }
+
+            ShowInputDialog = false;
+        }
+
+        private string BuildTaskInfo()
+        {
+            var taskInfoBuilder = new StringBuilder();
+
+            foreach (var task in Tasks)
+            {
+                taskInfoBuilder.AppendLine(
+                    string.Format("######################### | {0} | #########################", task.TaskModel.Title));
+                if (task.State == TaskState.Completed)
+                {
+                    taskInfoBuilder.AppendLine(string.Format("Total minutes: {0}", task.TaskModel.TotalMinutes));
+                }
+                else
+                {
+                    taskInfoBuilder.AppendLine(string.Format("Total minutes: -"));
+                }
+                taskInfoBuilder.AppendLine(string.Format("Start-End: {0} - {1}", task.TaskModel.Start, task.TaskModel.End));
+                taskInfoBuilder.AppendLine(string.Format("State: {0}", task.State));
+
+                if (task.TaskModel.Breaks.Any())
+                {
+                    int i = 0;
+                    foreach (var taskBreak in task.TaskModel.Breaks)
+                    {
+                        i++;
+                        taskInfoBuilder.AppendLine(string.Format("Break {0}: {1} minutes", i, string.Format("{0:0.##}", taskBreak.TotalMinutes)));
+                    }
+                }
+            }
+
+            return taskInfoBuilder.ToString();
         }
 
         #region PropertyChanged helper
